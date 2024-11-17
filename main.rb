ff3ee9c8103274c5f29d723b6c5c8a454df60eef
Thechuy1609 <blitzdashgd@gmail.com>
2024-11-16 21:16:58 -0500

    "done"

---
+++ main.rb
@@ -0,0 +1,28 @@
+def fibonacci_iteration(number)
+  array = [0,1,1]
+for i in 2..number do
+  if i == number
+    array.pop()
+  else
+  array << array[i] = array[i - 1] + array[i - 2]
+  end
+end
+array
+end
+
+
+def fibonacci_recursion(n)
+ puts 'This was printed recursively'
+ arr = [0,1,1,2]
+ if 
+
+
+ else
+  array = fibonacci_recursion(n -1)
+arr << array[-1] + array[-2]
+end
+
+
+
+def merge_sort()
+end
